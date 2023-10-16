95e73511bfa020b0cbf555140ff23e73028b8d14
Arnvid Karstad <arnvid@isd.no>
2023-10-17 00:30:31 +0200

    update: workaround for LFG icons not available character cannot perform role

--- Skin/Interface/FrameXML/LFGFrame.lua
+++ Skin/Interface/FrameXML/LFGFrame.lua
@@ -13,6 +13,9 @@ local Color, Util = Aurora.Color, Aurora.Util
 do --[[ FrameXML\LFGFrame.lua ]]
     function Hook.LFG_SetRoleIconIncentive(roleButton, incentiveIndex)
         local roleIcon = roleButton:GetNormalTexture()
+        if roleIcon._auroraBorder == nil then 
+            return 
+        end
         if incentiveIndex then
             roleIcon._auroraBorder:SetColorTexture(Color.yellow:GetRGB())
         else
