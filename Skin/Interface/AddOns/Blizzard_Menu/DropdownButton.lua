8478b437066e36ca8f057b826498f84414793fd5
Arnvid Karstad <arnvid@isd.no>
2024-11-11 00:16:16 +0100

    add:  Skin.DropdownButton to replace UIDropDownMenuTemplate

--- Skin/Interface/AddOns/Blizzard_Menu/DropdownButton.lua
+++ Skin/Interface/AddOns/Blizzard_Menu/DropdownButton.lua
@@ -7,35 +7,44 @@ if private.shouldSkip() then return end
 --[[ Core ]]
 local Aurora = private.Aurora
 local Skin =  Aurora.Skin
--- local Base = Aurora.Base
--- local Hook, Skin = Aurora.Hook, Aurora.Skin
--- local Color = Aurora.Color
+local Base = Aurora.Base
+local Hook, Skin = Aurora.Hook, Aurora.Skin
+local Color = Aurora.Color
+
 do --[[ Blizzard_Menu\DropdownButton.lua ]]
     do --[[ DropdownButton.lua ]]
         function Skin.DropdownButton(Frame, Width)
-            _G.print("Skin.DropdownButton")
-            -- if not Width then
-            --     Width = 155
-            -- end
-            -- if Frame.Arrow then
-            --     Frame.Arrow:SetAlpha(0)
-            -- end
-            -- if Frame.Left then
-            --     Frame.Left:SetAlpha(0)
-            -- end
-            -- if Frame.Middle then
-            --     Frame.Middle:SetAlpha(0)
-            -- end
-            -- if Frame.Right then
-            --     Frame.Right:SetAlpha(0)
-            -- end
-            -- Frame.backdrop:Point('TOPLEFT', 0, -2)
-            -- Frame.backdrop:Point('BOTTOMRIGHT', 0, 2)
-            local tex = Frame:CreateTexture(nil, 'ARTWORK')
-            tex:SetTexture([[Interface\AddOns\Aurora\media\arrow-down-active]])
-            tex:SetRotation(3.14)
-            -- tex:Point('RIGHT', Frame.backdrop, -3, 0)
-            -- tex:Size(14)
+            local rightOfs = -105
+            if not Frame then
+                -- _G.print("Skin.DropdownButton - Frame is nil")
+                return
+            end
+            if not Width then
+                Width = 155
+            end
+            if Frame.Left then
+                Frame.Left:SetAlpha(0)
+            end
+            if Frame.Middle then
+                Frame.Middle:SetAlpha(0)
+            end
+            if Frame.Right then
+                Frame.Right:SetAlpha(0)
+            end
+            Frame._auroraWidth = nil
+            Base.SetBackdrop(Frame, Color.button)
+            if Frame.Arrow then
+
+                Frame.Background:SetTexture(nil)
+                Frame:SetFrameLevel(Frame:GetFrameLevel() + 2)
+                Frame.Arrow:SetAlpha(0)
+                if Frame.TabHighlight then Frame.TabHighlight:SetAlpha(0) end
+                local tex = Frame:CreateTexture(nil, "ARTWORK")
+                tex:SetPoint("RIGHT", Frame, -3, 0)
+                tex:SetTexture([[Interface\AddOns\Aurora\media\arrow-down-active]])
+                tex:SetSize(13,13)
+                return
+            end
         end
     end
 end
