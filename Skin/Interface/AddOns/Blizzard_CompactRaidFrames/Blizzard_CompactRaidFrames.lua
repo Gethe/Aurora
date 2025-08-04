ee838eb64d565595678bd2165ab8b7f2ddd102d1
Arnvid Karstad <arnvid@isd.no>
2025-08-04 23:53:13 +0200

    chg: updates to Blizzard_CompactRaidFrames skin

--- Skin/Interface/AddOns/Blizzard_CompactRaidFrames/Blizzard_CompactRaidFrames.lua
+++ Skin/Interface/AddOns/Blizzard_CompactRaidFrames/Blizzard_CompactRaidFrames.lua
@@ -8,6 +8,7 @@ if private.shouldSkip() then return end
 local Aurora = private.Aurora
 local Base = Aurora.Base
 local Hook, Skin = Aurora.Hook, Aurora.Skin
+local Util = Aurora.Util
 
 do --[[ AddOns\Blizzard_CompactRaidFrames.lua ]]
     do --[[ Blizzard_CompactRaidFrameManager ]]
@@ -39,6 +40,24 @@ do --[[ AddOns\Blizzard_CompactRaidFrames.xml ]]
     end
 end
 
+do --[[ AddOns\Blizzard_CompactRaidFrames.lua ]]
+    Hook.CRFManagerFilterRoleButtonMixin = {}
+    -- function Hook.CRFManagerFilterRoleButtonMixin:Init()
+    --     _G.print("CRFManagerFilterRoleButtonMixin:Init - ", self:GetName())
+    -- end
+    function Hook.CRFManagerFilterRoleButtonMixin:OnEnter()
+       -- _G.print("CRFManagerFilterRoleButtonMixin:OnEnter - ", self:GetName())
+
+        -- if not button._auroraSkinned then
+        --     button._auroraSkinned = true
+        -- end
+    end
+    Hook.CRFManagerRaidIconButtonMixin = {}
+    -- function Hook.CRFManagerRaidIconButtonMixin:Init()
+    --     _G.print("CRFManagerRaidIconButtonMixin:Init - ", self:GetName())
+    -- end
+end
+
 function private.AddOns.Blizzard_CompactRaidFrames()
     ----====####$$$$%%%%%%%%%%%%%%%%%$$$$####====----
     -- Blizzard_CompactRaidFrameReservationManager --
@@ -52,68 +71,69 @@ function private.AddOns.Blizzard_CompactRaidFrames()
     -- Blizzard_CompactRaidFrameManager --
     ----====####$$$$%%%%%%$$$$####====----
     -- FIXLATER - disable for now
-    if private.isRetail then return end
+    -- if private.isRetail then return end
     _G.hooksecurefunc("CompactRaidFrameManager_Toggle", Hook.CompactRaidFrameManager_Toggle)
 
     local CompactRaidFrameManager = _G.CompactRaidFrameManager
     CompactRaidFrameManager:DisableDrawLayer("ARTWORK")
     Skin.FrameTypeFrame(CompactRaidFrameManager)
 
-    local toggleButton = CompactRaidFrameManager.toggleButton
-    toggleButton:SetPoint("RIGHT", -1, 0)
-    toggleButton:SetScript("OnMouseDown", private.nop)
-    toggleButton:SetScript("OnMouseUp", private.nop)
+    local toggleButtonForward = CompactRaidFrameManager.toggleButtonForward
+    toggleButtonForward:SetPoint("RIGHT", -1, 0)
+    toggleButtonForward:SetScript("OnMouseDown", private.nop)
+    toggleButtonForward:SetScript("OnMouseUp", private.nop)
 
-    local arrow = toggleButton:GetNormalTexture()
-    arrow:ClearAllPoints()
-    arrow:SetPoint("TOPLEFT", 3, -5)
-    arrow:SetPoint("BOTTOMRIGHT", -3, 5)
-    Base.SetTexture(arrow, "arrowRight")
+    local arrowForward = toggleButtonForward:GetNormalTexture()
+    arrowForward:ClearAllPoints()
+    arrowForward:SetPoint("TOPLEFT", 3, -5)
+    arrowForward:SetPoint("BOTTOMRIGHT", -3, 5)
+    Base.SetTexture(arrowForward, "arrowRight")
+
+
+    local toggleButtonBack = CompactRaidFrameManager.toggleButtonBack
+    toggleButtonBack:SetPoint("RIGHT", -1, 0)
+    toggleButtonBack:SetScript("OnMouseDown", private.nop)
+    toggleButtonBack:SetScript("OnMouseUp", private.nop)
+
+    local arrowBack = toggleButtonBack:GetNormalTexture()
+    arrowBack:ClearAllPoints()
+    arrowBack:SetPoint("TOPLEFT", 3, -5)
+    arrowBack:SetPoint("BOTTOMRIGHT", -3, 5)
+    Base.SetTexture(arrowBack, "arrowLeft")
 
     local displayFrame = CompactRaidFrameManager.displayFrame
     local displayFrameName = displayFrame:GetName()
     displayFrame:GetRegions():Hide()
-
-    --  FIXLATER removed
-    -- local headerDelineator = _G[displayFrameName.."HeaderDelineator"]
-    -- headerDelineator:SetColorTexture(1, 1, 1)
-    -- headerDelineator:SetPoint("TOPLEFT", 0, -27)
-    -- headerDelineator:SetPoint("TOPRIGHT", -7, -27)
-    -- headerDelineator:SetHeight(1)
-
     local optionsButton = _G[displayFrameName.."OptionsButton"]
+    -- CompactRaidFrameManagerDisplayFrameOptionsButton
     Skin.UIPanelInfoButton(optionsButton)
 
-    --  FIXLATER removed
-    -- displayFrame.optionsFlowContainer:SetPoint("TOPLEFT", headerDelineator, "BOTTOMLEFT", -10, -1)
-
-
-    local filterOptions = displayFrame.filterOptions
-    -- local footerDelineator = _G[filterOptions:GetName().."FooterDelineator"]
-    -- footerDelineator:SetColorTexture(1, 1, 1)
-    -- footerDelineator:SetPoint("BOTTOMLEFT", 0, 7)
-    -- footerDelineator:SetPoint("BOTTOMRIGHT", 4, 7)
-    -- footerDelineator:SetHeight(1)
-
-    -- Skin.CRFManagerFilterRoleButtonTemplate(filterOptions.filterRoleTank)
+    -- FIXLATER     
+    Util.Mixin(_G.CRFManagerFilterRoleButtonMixin, Hook.CRFManagerFilterRoleButtonMixin)
+    Util.Mixin(_G.CRFManagerRaidIconButtonMixin, Hook.CRFManagerRaidIconButtonMixin)
+    -- CRFManagerRoleMarkerCheckMixin
+    -- CRFManagerFilterGroupButtonMixin
+    -- FIXLATER - disable for now
+    -- local filterOptions = displayFrame.filterOptions
+    -- Skin.CRFManagerFilterRoleButtonTemplate(filterOptions["filterRoleTank"])
     -- Skin.CRFManagerFilterRoleButtonTemplate(filterOptions.filterRoleHealer)
     -- Skin.CRFManagerFilterRoleButtonTemplate(filterOptions.filterRoleDamager)
-    for i = 1, 8 do
-        Skin.CRFManagerFilterGroupButtonTemplate(filterOptions["filterGroup"..i])
-    end
+    -- for i = 1, 8 do
+    --     Skin.CRFManagerFilterRoleButtonTemplate(filterOptions["filterGroup"..i])
+    -- end
 
-    Skin.UIMenuButtonStretchTemplate(displayFrame.editMode)
-    Skin.UIMenuButtonStretchTemplate(displayFrame.hiddenModeToggle)
-    Skin.UIMenuButtonStretchTemplate(displayFrame.convertToRaid)
+    -- Skin.UIMenuButtonStretchTemplate(displayFrame.editMode)
+    -- Skin.UIMenuButtonStretchTemplate(displayFrame.hiddenModeToggle) -> RaidFrameHiddenModeToggleMixin = CreateFromMixins(CRFManagerTooltipButtonMixin);
+    -- Skin.UIMenuButtonStretchTemplate(displayFrame.convertToRaid)
 
-    local icons = {displayFrame.raidMarkers:GetChildren()}
-    for i, icon in next, icons do
-        Skin.CRFManagerRaidIconButtonTemplate(icon)
-    end
+    -- local icons = {displayFrame.raidMarkers:GetChildren()}
+    -- for i, icon in next, icons do
+    --     Skin.CRFManagerRaidIconButtonTemplate(icon)
+    -- end
 
-    local leaderOptions = displayFrame.leaderOptions
-    Skin.UIMenuButtonStretchTemplate(leaderOptions.rolePollButton)
-    Skin.UIMenuButtonStretchTemplate(leaderOptions.readyCheckButton)
-    Skin.UIMenuButtonStretchTemplate(_G[leaderOptions:GetName().."RaidWorldMarkerButton"])
-    Skin.UICheckButtonTemplate(displayFrame.everyoneIsAssistButton)
+    -- local leaderOptions = displayFrame.leaderOptions
+    -- Skin.UIMenuButtonStretchTemplate(leaderOptions.rolePollButton) -> displayFrame.rolePollButton:Enable();
+    -- Skin.UIMenuButtonStretchTemplate(leaderOptions.readyCheckButton) -> displayFrame.readyCheckButton
+    -- Skin.UIMenuButtonStretchTemplate(_G[leaderOptions:GetName().."RaidWorldMarkerButton"]) -- removewd
+    -- Skin.UICheckButtonTemplate(displayFrame.everyoneIsAssistButton) -> <CheckButton name="$parentEveryoneIsAssistButton" parentKey="everyoneIsAssistButton" hidden="true" mixin="RaidFrameEveryoneIsAssistMixin">
 end
