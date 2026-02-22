376e19b863af49b4065820d8905655cd08a95ed7
Arnvid Karstad <arnvid@isd.no>
2026-02-22 16:51:53 +0100

    fix: ZoneAbilityFrameTemplate

--- Skin/Interface/AddOns/Blizzard_ZoneAbility/Blizzard_ZoneAbility.lua
+++ Skin/Interface/AddOns/Blizzard_ZoneAbility/Blizzard_ZoneAbility.lua
@@ -7,8 +7,7 @@ if private.shouldSkip() then return end
 --[[ Core ]]
 local Aurora = private.Aurora
 local Base = Aurora.Base
-local Hook, Skin = Aurora.Hook, Aurora.Skin
-local Util = Aurora.Util
+local Skin = Aurora.Skin
 
 --do --[[ FrameXML\ZoneAbility.lua ]]
 --end
@@ -16,7 +15,8 @@ local Util = Aurora.Util
 do --[[ FrameXML\ZoneAbility.xml ]]
     function Skin.ZoneAbilityFrameTemplate(Frame)
         Frame.Style:Hide()
-        Util.Mixin(Frame.SpellButtonContainer.contentFramePool, Hook.ObjectPoolMixin)
+        -- contentFramePool is created by ManagedLayoutFrameMixin:SetTemplate during OnLoad.
+        -- Hook.ObjectPoolMixin removed in 11.0.0; hook the button mixin's OnLoad instead.
     end
     function Skin.ZoneAbilityFrameSpellButtonTemplate(Button)
         --Button.Icon:SetTexture(private.textures.plain)
@@ -34,4 +34,5 @@ end
 
 function private.FrameXML.ZoneAbility()
     Skin.ZoneAbilityFrameTemplate(_G.ZoneAbilityFrame)
+    _G.hooksecurefunc(_G.ZoneAbilityFrameSpellButtonMixin, "OnLoad", Skin.ZoneAbilityFrameSpellButtonTemplate)
 end
