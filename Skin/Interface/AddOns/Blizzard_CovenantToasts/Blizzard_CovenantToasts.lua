c9087e24a40ef85eb8a00136b28b8aa06f2b3ec7
Arnvid Karstad <arnvid@isd.no>
2026-04-14 08:51:43 +0200

    add: skin for Blizzard_CovenantToasts (covenant choice and renown toasts)

---
+++ Skin/Interface/AddOns/Blizzard_CovenantToasts/Blizzard_CovenantToasts.lua
@@ -0,0 +1,87 @@
+local _, private = ...
+if private.shouldSkip() then return end
+
+local Aurora = private.Aurora
+local Base, Hook = Aurora.Base, Aurora.Hook
+local Color = Aurora.Color
+
+do --[[ AddOns\Blizzard_CovenantToasts.lua ]]
+    -- CovenantRenownToastMixin:SetupRewardVisuals dynamically sets the
+    -- RewardIcon texture. Hook it to crop the icon each time it changes.
+    Hook.CovenantRenownToastMixin = {}
+    function Hook.CovenantRenownToastMixin:SetupRewardVisuals()
+        if self.RewardIcon and self.RewardIcon:IsShown() then
+            Base.CropIcon(self.RewardIcon)
+        end
+    end
+end
+
+do --[[ AddOns\Blizzard_CovenantToasts.xml ]]
+end
+
+function private.AddOns.Blizzard_CovenantToasts()
+    ------------------------------------------------
+    -- CovenantChoiceToast
+    -- Inherits CovenantCelebrationBannerTemplate
+    -- Children: ToastBG, Stars1, Stars2, HeaderText, CovenantName
+    -- Banner base: GlowLineTop, GlowLineTopAdditive, IconSwirlModelScene, Icon.Tex
+    ------------------------------------------------
+    local choiceToast = _G.CovenantChoiceToast
+    if choiceToast and not choiceToast._auroraSkinned then
+        choiceToast._auroraSkinned = true
+
+        -- Strip decorative atlas textures
+        if choiceToast.ToastBG then
+            choiceToast.ToastBG:SetAlpha(0)
+        end
+        if choiceToast.GlowLineTop then
+            choiceToast.GlowLineTop:SetAlpha(0)
+        end
+        if choiceToast.GlowLineTopAdditive then
+            choiceToast.GlowLineTopAdditive:SetAlpha(0)
+        end
+
+        -- Apply backdrop to the toast frame
+        Base.SetBackdrop(choiceToast, Color.frame)
+    end
+
+    ------------------------------------------------
+    -- CovenantRenownToast
+    -- Inherits CovenantCelebrationBannerTemplate
+    -- Children: ToastBG, Stars1, Stars2, RenownLabel, RewardIcon,
+    --           RewardIconMask, RewardIconRing, RewardDescription,
+    --           GlowLineTopBottom, RewardIconMouseOver
+    -- Banner base: GlowLineTop, GlowLineTopAdditive, IconSwirlModelScene, Icon.Tex
+    ------------------------------------------------
+    local renownToast = _G.CovenantRenownToast
+    if renownToast and not renownToast._auroraSkinned then
+        renownToast._auroraSkinned = true
+
+        -- Strip decorative atlas textures
+        if renownToast.ToastBG then
+            renownToast.ToastBG:SetAlpha(0)
+        end
+        if renownToast.GlowLineTop then
+            renownToast.GlowLineTop:SetAlpha(0)
+        end
+        if renownToast.GlowLineTopAdditive then
+            renownToast.GlowLineTopAdditive:SetAlpha(0)
+        end
+        if renownToast.GlowLineTopBottom then
+            renownToast.GlowLineTopBottom:SetAlpha(0)
+        end
+
+        -- Apply backdrop to the toast frame
+        Base.SetBackdrop(renownToast, Color.frame)
+
+        -- Crop the reward icon (dynamically set via SetupRewardVisuals)
+        if renownToast.RewardIcon then
+            Base.CropIcon(renownToast.RewardIcon)
+        end
+
+        -- Hook SetupRewardVisuals to re-crop the icon when it changes
+        if CovenantRenownToastMixin then
+            _G.hooksecurefunc(CovenantRenownToastMixin, "SetupRewardVisuals", Hook.CovenantRenownToastMixin.SetupRewardVisuals)
+        end
+    end
+end
