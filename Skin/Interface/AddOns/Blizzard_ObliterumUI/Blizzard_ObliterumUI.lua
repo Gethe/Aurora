1fd52e5554175b0bde17cb2d7e986ae3d979aa1f
Arnvid Karstad <arnvid@isd.no>
2026-04-14 08:49:59 +0200

    add: skin for Blizzard_ObliterumUI (Legion obliterum forge)

---
+++ Skin/Interface/AddOns/Blizzard_ObliterumUI/Blizzard_ObliterumUI.lua
@@ -0,0 +1,53 @@
+local _, private = ...
+if private.shouldSkip() then return end
+
+local Aurora = private.Aurora
+local Base, Hook, Skin = Aurora.Base, Aurora.Hook, Aurora.Skin
+
+do --[[ AddOns\Blizzard_ObliterumUI.lua ]]
+end
+
+do --[[ AddOns\Blizzard_ObliterumUI.xml ]]
+end
+
+function private.AddOns.Blizzard_ObliterumUI()
+    local frame = _G.ObliterumForgeFrame
+    if not frame then return end
+
+    ------------------------------------
+    -- Main frame (ButtonFrameTemplate)
+    ------------------------------------
+    Skin.FrameTypeFrame(frame)
+
+    ------------------------------------
+    -- Strip decorative textures
+    ------------------------------------
+    -- Background atlas (obliterumforge-background)
+    if frame.Background then
+        frame.Background:SetAlpha(0)
+    end
+
+    ------------------------------------
+    -- Item slot icon
+    ------------------------------------
+    local ItemSlot = frame.ItemSlot
+    if ItemSlot then
+        if ItemSlot.Icon then
+            Base.CropIcon(ItemSlot.Icon, ItemSlot)
+        end
+        -- Hide decorative corner textures
+        if ItemSlot.Corners then
+            ItemSlot.Corners:SetAlpha(0)
+        end
+        if ItemSlot.GlowCorners then
+            ItemSlot.GlowCorners:SetAlpha(0)
+        end
+    end
+
+    ------------------------------------
+    -- Obliterate action button
+    ------------------------------------
+    if frame.ObliterateButton then
+        Skin.FrameTypeButton(frame.ObliterateButton)
+    end
+end
